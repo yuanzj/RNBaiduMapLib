df888d8e5dd97647941d3c2074bfe435646bfb72
yuanzhijian <zhijian.yuan@rokyinfo.com>
2018-11-15 15:19:14 +0800

    1、增加wgs84转bd09坐标系

--- BaiduMapDemo.js
+++ BaiduMapDemo.js
@@ -2,177 +2,207 @@
  * @author lovebing
  */
 
-import React, {
-  Component,
-  PropTypes
-} from 'react';
+import React, { Component, PropTypes } from 'react';
 
 import MapTypes from './MapTypes';
 import MapView from './MapView';
 import Geolocation from './Geolocation';
 
 import {
-  Button,
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight,
-  NativeModules,
-  Platform,
-  DeviceEventEmitter
+	Button,
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
+	TouchableHighlight,
+	NativeModules,
+	Platform,
+	DeviceEventEmitter
 } from 'react-native';
 
 import Dimensions from 'Dimensions';
 
 export default class BaiduMapDemo extends Component {
+	constructor() {
+		super();
 
-  constructor() {
-    super();
+		this.state = {
+			mayType: MapTypes.NORMAL,
+			zoom: 15,
+			center: {
+				longitude: 113.981718,
+				latitude: 22.542449
+			},
+			myLocationEnabled: true,
+			trafficEnabled: false,
+			baiduHeatMapEnabled: false,
+			markers: [
+				{
+					longitude: 113.989718,
+					latitude: 22.542449,
+					coordType: 'wgs84',
+					title: 'Window of the world'
+				},
+				{
+					longitude: 113.998516,
+					latitude: 22.537642,
+					coordType: 'wgs84',
+					title: ''
+				}
+			],
+			clusterMarkers: [
+				{
+					longitude: 113.981718,
+					latitude: 22.542449,
+					coordType: 'wgs84',
+					clusterSize: 5,
+					title: 'Window of the world'
+				},
+				{
+					longitude: 113.995516,
+					latitude: 22.537642,
+					coordType: 'wgs84',
+					clusterSize: 100,
+					title: ''
+				}
+			]
+		};
+	}
 
-    this.state = {
-      mayType: MapTypes.NORMAL,
-      zoom: 15,
-      center: {
-        longitude: 113.981718,
-        latitude: 22.542449
-      },
-      myLocationEnabled: true,
-      trafficEnabled: false,
-      baiduHeatMapEnabled: false,
-      markers: [{
-        longitude: 113.989718,
-        latitude: 22.542449,
-        title: "Window of the world"
-      },{
-        longitude: 113.998516,
-        latitude: 22.537642,
-        title: ""
-      }],
-      clusterMarkers: [{
-        longitude: 113.981718,
-        latitude: 22.542449,
-        clusterSize: 5,
-        title: "Window of the world"
-      },{
-        longitude: 113.995516,
-        latitude: 22.537642,
-        clusterSize: 100,
-        title: ""
-      }]
-    };
-  }
+	componentDidMount() {}
 
-  componentDidMount() {
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<MapView
+					myLocationEnabled={this.state.myLocationEnabled}
+					trafficEnabled={this.state.trafficEnabled}
+					baiduHeatMapEnabled={this.state.baiduHeatMapEnabled}
+					zoom={this.state.zoom}
+					mapType={this.state.mapType}
+					center={this.state.center}
+					marker={this.state.marker}
+					markers={this.state.markers}
+					clusterMarkers={this.state.clusterMarkers}
+					style={styles.map}
+					onMarkerClick={(e) => {
+						console.warn(JSON.stringify(e));
+					}}
+					onMapClick={(e) => {}}
+					onMapStatusChangeFinish={(e) => {
+						console.warn(JSON.stringify(e));
+					}}
+				/>
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <MapView 
-          myLocationEnabled={this.state.myLocationEnabled}
-          trafficEnabled={this.state.trafficEnabled}
-          baiduHeatMapEnabled={this.state.baiduHeatMapEnabled}
-          zoom={this.state.zoom}
-          mapType={this.state.mapType}
-          center={this.state.center}
-          marker={this.state.marker}
-          markers={this.state.markers}
-          clusterMarkers={this.state.clusterMarkers}
-          style={styles.map} 
-          onMarkerClick={(e) => {
-            console.warn(JSON.stringify(e));
-          }}
-          onMapClick={(e) => {
-          }}
-        >
-        </MapView>
+				<View style={styles.row}>
+					<Button
+						title="Normal"
+						onPress={() => {
+							this.setState({
+								mapType: MapTypes.NORMAL
+							});
+						}}
+					/>
+					<Button
+						style={styles.btn}
+						title="Satellite"
+						onPress={() => {
+							this.setState({
+								mapType: MapTypes.SATELLITE
+							});
+						}}
+					/>
 
-        <View style={styles.row}>
-          <Button title="Normal" onPress={() => {
-            this.setState({
-              mapType: MapTypes.NORMAL
-            });
-          }} />
-          <Button style={styles.btn} title="Satellite" onPress={() => {
-            this.setState({
-              mapType: MapTypes.SATELLITE
-            });
-          }} />
+					<Button
+						style={styles.btn}
+						title="Locate"
+						onPress={() => {
+							console.warn('center', this.state.center);
+							Geolocation.getCurrentPosition()
+								.then((data) => {
+									console.warn(JSON.stringify(data));
+									this.setState({
+										zoom: 15,
+										marker: {
+											latitude: data.latitude,
+											longitude: data.longitude,
+											coordType: 'bd09',
+											title: 'Your location'
+										},
+										center: {
+											latitude: data.latitude,
+											longitude: data.longitude,
+											coordType: 'bd09',
+											rand: Math.random()
+										}
+									});
+								})
+								.catch((e) => {
+									console.warn(e, 'error');
+								});
+						}}
+					/>
+				</View>
 
-          <Button style={styles.btn} title="Locate" onPress={() => {
-              console.warn('center', this.state.center);
-              Geolocation.getCurrentPosition()
-                .then(data => {
-                  console.warn(JSON.stringify(data));
-                  this.setState({
-                    zoom: 15,
-                    marker: {
-                      latitude: data.latitude,
-                      longitude: data.longitude,
-                      title: 'Your location'
-                    },
-                    center: {
-                      latitude: data.latitude,
-                      longitude: data.longitude,
-                      rand: Math.random()
-                    }
-                  });
-                })
-                .catch(e =>{
-                  console.warn(e, 'error');
-                })
-          }} />
-        </View>
+				<View style={styles.row}>
+					<Button
+						title="Zoom+"
+						onPress={() => {
+							this.setState({
+								zoom: this.state.zoom + 1
+							});
+						}}
+					/>
+					<Button
+						title="Zoom-"
+						onPress={() => {
+							if (this.state.zoom > 0) {
+								this.setState({
+									zoom: this.state.zoom - 1
+								});
+							}
+						}}
+					/>
+				</View>
 
-        <View style={styles.row}>
-          <Button title="Zoom+" onPress={() => {
-            this.setState({
-              zoom: this.state.zoom + 1
-            });
-          }} />
-          <Button title="Zoom-" onPress={() => {
-            if(this.state.zoom > 0) {
-              this.setState({
-                zoom: this.state.zoom - 1
-              });
-            }
-            
-          }} />
-        </View>
+				<View style={styles.row}>
+					<Button
+						title="Traffic"
+						onPress={() => {
+							this.setState({
+								trafficEnabled: !this.state.trafficEnabled
+							});
+						}}
+					/>
 
-        <View style={styles.row}>
-          <Button title="Traffic" onPress={() => {
-            this.setState({
-              trafficEnabled: !this.state.trafficEnabled
-            });
-          }} />
-
-          <Button title="Baidu HeatMap" onPress={() => {
-            this.setState({
-              baiduHeatMapEnabled: !this.state.baiduHeatMapEnabled
-            });
-          }} />
-        </View>
-      </View>
-    );
-  }
+					<Button
+						title="Baidu HeatMap"
+						onPress={() => {
+							this.setState({
+								baiduHeatMapEnabled: !this.state.baiduHeatMapEnabled
+							});
+						}}
+					/>
+				</View>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  row: {
-    flexDirection: 'row',
-    height: 40
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height - 200,
-    marginBottom: 16
-  }
+	row: {
+		flexDirection: 'row',
+		height: 40
+	},
+	container: {
+		flex: 1,
+		justifyContent: 'flex-start',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF'
+	},
+	map: {
+		width: Dimensions.get('window').width,
+		height: Dimensions.get('window').height - 200,
+		marginBottom: 16
+	}
 });
--- android/app/src/main/java/net/rokyinfo/rn/baidumap/MarkerUtil.java
+++ android/app/src/main/java/net/rokyinfo/rn/baidumap/MarkerUtil.java
@@ -7,6 +7,7 @@ import com.baidu.mapapi.map.Marker;
 import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.utils.CoordinateConverter;
 import com.facebook.react.bridge.ReadableMap;
 
 /**
@@ -51,9 +52,28 @@ public class MarkerUtil {
     }
 
     public static LatLng getLatLngFromOption(ReadableMap option) {
+
         double latitude = option.getDouble("latitude");
         double longitude = option.getDouble("longitude");
-        return new LatLng(latitude, longitude);
 
+        if (option.getString("coordType").equals("wgs84")) {
+            // 将GPS设备采集的原始GPS坐标转换成百度坐标
+            CoordinateConverter converter = new CoordinateConverter();
+            converter.from(CoordinateConverter.CoordType.GPS);
+
+            // sourceLatLng待转换坐标
+            converter.coord(new LatLng(latitude, longitude));
+            return converter.convert();
+        } else if (option.getString("coordType").equals("gcj02")) {
+            // 国测局GPS坐标转换成百度坐标
+            CoordinateConverter converter = new CoordinateConverter();
+            converter.from(CoordinateConverter.CoordType.COMMON);
+
+            // sourceLatLng待转换坐标
+            converter.coord(new LatLng(latitude, longitude));
+            return converter.convert();
+        } else {
+            return new LatLng(latitude, longitude);
+        }
     }
 }
